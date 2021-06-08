19d07ae9282cef81fe863c275e13563419ee8101
ryanhe312 <ryanhe312@gmail.com>
2021-06-08 16:18:17 +0800

    render 3d with uv_tex

--- utils/render.py
+++ utils/render.py
@@ -26,8 +26,33 @@ cfg = {
 
 render_app = RenderPipeline(**cfg)
 
+def bilinear_interpolate(img, x, y):
+    """
+    https://stackoverflow.com/questions/12729228/simple-efficient-bilinear-interpolation-of-images-in-numpy-and-python
+    """
+    x0 = np.floor(x).astype(np.int32)
+    x1 = x0 + 1
+    y0 = np.floor(y).astype(np.int32)
+    y1 = y0 + 1
 
-def render(img, ver_lst, tri, alpha=0.6, show_flag=False, wfp=None, with_bg_flag=True):
+    x0 = np.clip(x0, 0, img.shape[1] - 1)
+    x1 = np.clip(x1, 0, img.shape[1] - 1)
+    y0 = np.clip(y0, 0, img.shape[0] - 1)
+    y1 = np.clip(y1, 0, img.shape[0] - 1)
+
+    i_a = img[y0, x0]
+    i_b = img[y1, x0]
+    i_c = img[y0, x1]
+    i_d = img[y1, x1]
+
+    wa = (x1 - x) * (y1 - y)
+    wb = (x1 - x) * (y - y0)
+    wc = (x - x0) * (y1 - y)
+    wd = (x - x0) * (y - y0)
+
+    return wa[..., np.newaxis] * i_a + wb[..., np.newaxis] * i_b + wc[..., np.newaxis] * i_c + wd[..., np.newaxis] * i_d
+
+def render(img, ver_lst, tri, alpha=0.6, show_flag=False, wfp=None, with_bg_flag=False):
     if with_bg_flag:
         overlap = img.copy()
     else:
@@ -35,7 +60,8 @@ def render(img, ver_lst, tri, alpha=0.6, show_flag=False, wfp=None, with_bg_flag
 
     for ver_ in ver_lst:
         ver = _to_ctype(ver_.T)  # transpose
-        overlap = render_app(ver, tri, overlap)
+        colors = bilinear_interpolate(img, ver[:, 0], ver[:, 1]) / 255.
+        overlap = render_app(ver, tri, overlap, colors)
 
     if with_bg_flag:
         res = cv2.addWeighted(img, 1 - alpha, overlap, alpha, 0)
